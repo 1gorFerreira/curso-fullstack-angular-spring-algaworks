21a1d4d19cda849789d1c92b53d179552e9885e2
Igor <igorferreiradaniel99@hotmail.com>
2023-06-27 10:51:52 -0300

    Registering new category with POST

--- src/main/java/com/algaworks/algamoney/api/controller/CategoryController.java
+++ src/main/java/com/algaworks/algamoney/api/controller/CategoryController.java
@@ -2,11 +2,13 @@ package com.algaworks.algamoney.api.controller;
 
 import com.algaworks.algamoney.api.model.Category;
 import com.algaworks.algamoney.api.respository.CategoryRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,6 +21,20 @@ public class CategoryController {
     @GetMapping
     public List<Category> listAll(){
         return categoryRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> create(@RequestBody Category category, HttpServletResponse response){
+        Category categorySaved = categoryRepository.save(category);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(categorySaved.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(categorySaved);
+    }
+
+    @GetMapping("/{id}")
+    public Category findById(@PathVariable  Long id){
+        return categoryRepository.findById(id).orElse(null);
     }
 
 }
