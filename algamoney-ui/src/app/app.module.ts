82c4007ed7049ce15d45d86285d648a1396851a8
igor <igorferreiradaniel99@hotmail.com>
2023-11-29 09:17:54 -0300

    Mod 10.4 - Adding the release search form

--- algamoney-ui/src/app/app.module.ts
+++ algamoney-ui/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TabViewModule } from 'primeng/tabview';
+import { InputTextModule } from 'primeng/inputtext';
+import { Button, ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    TabViewModule,
+    ButtonModule,
+    InputTextModule,
     BrowserModule,
     AppRoutingModule
   ],
